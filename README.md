b4d229432e37741fd1a510f6ee8aed805337d6da
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2025-01-21 10:49:35 +1100

    transaction schema and model create

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ This is the backend service for the Finance Tracker application. It provides API
 - Node.js
 - Express.js
 - MongoDB
+- JWT for Authentication
 - Bcrypt
 - Cors
 - Dotenv
--- server.js
+++ server.js
@@ -3,14 +3,18 @@ import cors from "cors"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 import connectMongodb from "./mongodbConfig.js"
 
 dotenv.config()
+connectMongodb()
+
 const app = express()
 const PORT = process.env.PORT || 5050
 
-connectMongodb()
-
+//
+// -------------------------User Schema and Model
+//
 const userSchema = mongoose.Schema(
   {
     username: {
@@ -30,23 +34,55 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 )
-
 const User = mongoose.model("user", userSchema)
+
+//
+// --------------------------Transaction Schema and Model
+//
+
+const transactionSchema = mongoose.Schema(
+  {
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expence"],
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+const Transaction = mongoose.model("Transaction", transactionSchema)
 
 app.use(express.json())
 app.use(cors())
 
-// Server Live Message
 app.get("/", (req, res) => {
   res.json({
     message: "Server Is Live",
   })
 })
 
-
-
+//
 //------------------------------------ Create new User
-
+//
 
 app.post("/api/v1/users/signup", async (req, res) => {
   try {
@@ -73,10 +109,9 @@ app.post("/api/v1/users/signup", async (req, res) => {
   }
 })
 
-
-
+//
 // -------------------------------Login and Verification
-
+//
 
 app.post("/api/v1/users/login", async (req, res) => {
   try {
@@ -87,10 +122,19 @@ app.post("/api/v1/users/login", async (req, res) => {
       console.log(userData)
       const isLoginSuccess = await bcrypt.compare(password, userData.password)
 
+      const tokendata = {
+        email: userData.email,
+      }
+
+      const token = await jwt.sign(tokendata, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRESIN,
+      })
+
       if (isLoginSuccess) {
         res.status(200).json({
           status: "success",
           message: "login Successful",
+          accessToken: token,
         })
       } else {
         res.status(403).json({
@@ -113,7 +157,22 @@ app.post("/api/v1/users/login", async (req, res) => {
   }
 })
 
-
+//
+// ---------------------------------TO create a transaction
+//
+app.post("/api/v1/transaction", async (req, res) => {
+  try {
+    res.status(200).json({
+      status: "Scuuess",
+      message: "transaction added",
+    })
+  } catch (error) {
+    res.status(500).json({
+      status: "Error",
+      message: "Something went wrong",
+    })
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server Started AT PORT https://localhost:${PORT} `)
